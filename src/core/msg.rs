31a3e3d2c67fb116c26c7574bf2819fe84be98ad
mtcw99 <15316579+mtcw99@users.noreply.github.com>
2019-09-21 00:20:05 +0100

    init update

--- src/core/msg.rs
+++ src/core/msg.rs
@@ -3,15 +3,16 @@ use yansi::Paint;
 pub fn help() {
     print!("mssg - Mart's Static Site Generator\n\
             \n\
-            new  Create a new site project\n\
-            init Create a new site project on the current directory\n\
+            init Initialize a new site project\n\
             gen  Generate the static site\n\
             help Display this help message\n");
 }
 
-pub fn err_handle(msg: &'static str) {
+pub fn err_handle(msg: &'static str, print_help: bool) {
     eprintln!("{}: {}", Paint::red("ERROR").bold(), msg);
-    self::help();
+    if print_help == true {
+        self::help();
+    }
 }
 
 
