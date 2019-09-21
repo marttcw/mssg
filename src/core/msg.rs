1014cf650b6cf26ad1ce0413849cc8b2eb4cdee1
mtcw99 <15316579+mtcw99@users.noreply.github.com>
2019-09-21 13:38:05 +0100

    20190921 update

--- src/core/msg.rs
+++ src/core/msg.rs
@@ -5,7 +5,15 @@ pub fn help() {
             \n\
             init Initialize a new site project\n\
             gen  Generate the static site\n\
-            help Display this help message\n");
+            help Display this help message\n\
+            ver  Display version of the program\n\
+            host Host the site (Not supported yet)\n");
+}
+
+pub fn version() {
+    print!("mssg - Mart's Static Site Generator\n\
+            \n\
+            v0.0.1 - PRE-ALPHA release - 2019-09-21\n");
 }
 
 pub fn err_handle(msg: &'static str, print_help: bool) {
