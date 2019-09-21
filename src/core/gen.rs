08a66dcc11145d65c4144b56cd4e92659de1819d
martin <mtcw@disroot.org>
2019-09-21 14:09:50 +0100

    very basic generation

--- src/core/gen.rs
+++ src/core/gen.rs
@@ -4,23 +4,27 @@ use toml::Value;
 
 // Generate the site
 pub fn gen() -> std::io::Result<()> {
-    // Set base directories
-    let _gen_dir = Path::new("build");
-    let _src_dir = Path::new("src");
-
-    // Go to src directory
-    assert!(env::set_current_dir(&_src_dir).is_ok());
-    markdown("src/index.md")?;
+    markdown("src/index.md", "build/index.html")?;
 
     Ok(())
 }
 
-fn markdown(filename: &str) -> std::io::Result<()> {
+/*
+fn list_dirs(_list: Vec<String>) -> std::io::Result<()> {
+    // Set base directories
+    let _gen_dir = Path::new("build");
+    let _src_dir = Path::new("src");
+
+    Ok(())
+}
+*/
+
+fn markdown(in_filename: &str, out_filename: &str) -> std::io::Result<()> {
     let mut html_output = String::new();
 
     // Open file and read it to string
-    let file = fs::File::open(filename)?;
-    let mut buf_reader = BufReader::new(file);
+    let in_file = fs::File::open(in_filename)?;
+    let mut buf_reader = BufReader::new(in_file);
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents)?;
 
@@ -30,7 +34,8 @@ fn markdown(filename: &str) -> std::io::Result<()> {
     let parser = Parser::new_ext(&*contents, options);
     html::push_html(&mut html_output, parser);
 
-    print!("{}", html_output);
+    // Write html_output to file
+    fs::write(out_filename, html_output).expect("Unable to write file");
 
     Ok(())
 }
