6967b1e5c6a6166a703509fee7211ce6dea30f05
mtcw99 <15316579+mtcw99@users.noreply.github.com>
2019-09-22 01:53:19 +0100

    use clap for cli args

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,48 @@
-use std::env;
+use yansi::Paint;
 
 mod core;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+#[macro_use]
+extern crate clap;
 
-    match args.len() {
-        1 => core::msg::err_handle("Need at least one argument!", true),
-        _ => {
-            match &*args[1] {
-                "help" => core::msg::help(),
-                "ver"  => core::msg::version(),
-                "init" => {
-                    match args.len() {
-                        2 => core::new::init("").unwrap(),
-                        _ => core::new::init(&*args[2]).unwrap(),
-                    }
-                }
-                "gen" => core::gen::gen().unwrap(),
-                _ => core::msg::err_handle("Non-valid argument!", true),
-            }
+pub fn err_handle(msg: &'static str) {
+    eprintln!("{}: {}", Paint::red("ERROR").bold(), msg);
+}
+
+fn main() {
+    let matches = clap_app!(mssg =>
+        (version: crate_version!())
+        (author: crate_authors!())
+        (about: "Mart's Static Site Generator written in Rust (v2018)")
+        (@subcommand init =>
+            (about: "Initialize a new site project")
+            (@arg PROJ_NAME: "Name of the project")
+        )
+        (@subcommand gen =>
+            (about: "Generate the static site")
+        )
+        (@subcommand blog =>
+            (about: "Manage your blog")
+        )
+        (@subcommand host =>
+            (about: "Host your site")
+        )
+        (@setting ArgRequiredElseHelp)
+    ).get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("init") {
+        match matches.is_present("PROJ_NAME") {
+            true => core::new::init(matches.value_of("PROJ_NAME").unwrap()).unwrap(),
+            false => core::new::init("").unwrap(),
         }
+    } else if let Some(_matches) = matches.subcommand_matches("gen") {
+        core::gen::gen().unwrap();
+    } else if let Some(_matches) = matches.subcommand_matches("blog") {
+        /*
+            "post" => err_handle("Not supported at the moment", false),
+            "edit" => err_handle("Not supported at the moment", false),
+            "list" => err_handle("Not supported at the moment", false),
+            "delete" => err_handle("Not supported at the moment", false),
+        */
     }
 }
