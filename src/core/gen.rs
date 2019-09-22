8284dcfae81a9c3d8bda3c6f940cee835aa353c8
mtcw99 <15316579+mtcw99@users.noreply.github.com>
2019-09-22 13:42:00 +0100

    toml used, more deps

--- src/core/gen.rs
+++ src/core/gen.rs
@@ -1,29 +1,79 @@
-use std::{fs, env, io::BufReader, io::prelude::*, path::Path};
+use std::{fs, env, io::BufReader, io::prelude::*, path::Path, error::Error};
+use walkdir::{DirEntry, WalkDir};
 use pulldown_cmark::{Parser, Options, html};
 use toml::Value;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Config {
+    title: String,
+    description: String,
+    language: String,
+    global: Global_Config,
+}
+
+#[derive(Deserialize)]
+struct Global_Config {
+    order: Vec<String>
+}
+
+struct path_file {
+    rel_path: String,
+    filename: String
+}
+
+struct build_map_to {
+    build_file: String,
+    src_files_list: Vec<String>
+}
 
 // Generate the site
-pub fn gen() -> std::io::Result<()> {
-    markdown("src/index.md", "build/index.html")?;
+pub fn gen() -> Result<(), Box<dyn Error>> {
+    let mut list: Vec<path_file> = Vec::new();
+    let mut equiv: Vec<build_map_to> = Vec::new();
+
+    get_list_files(&mut list, "src")?;
+
+    for f in &list {
+        println!("{} : {}", f.rel_path, f.filename);
+    }
+
+    markdown("src/index.md", "src/config.toml", "build/index.html")?;
 
     Ok(())
 }
 
-/*
-fn list_dirs(_list: Vec<String>) -> std::io::Result<()> {
-    // Set base directories
-    let _gen_dir = Path::new("build");
-    let _src_dir = Path::new("src");
+// Walk directory traversal
+fn is_not_hidden(entry: &DirEntry) -> bool {
+    entry
+         .file_name()
+         .to_str()
+         .map(|s| entry.depth() == 0 || !s.starts_with("."))
+         .unwrap_or(false)
+}
+
+fn get_list_files(list: &mut Vec<path_file>, base_dir_path: &str) -> Result<(), Box<dyn Error>> {
+    WalkDir::new(base_dir_path)
+        .into_iter()
+        .filter_entry(|e| is_not_hidden(e))
+        .filter_map(|v| v.ok())
+        .for_each(|x| 
+            list.push(
+                path_file{
+                    rel_path: x.path().display().to_string(),
+                    filename: x.path().file_name().unwrap().to_os_string().into_string().unwrap()
+                }));
 
     Ok(())
 }
-*/
 
-fn markdown(in_filename: &str, out_filename: &str) -> std::io::Result<()> {
+// Markdown generator
+fn markdown(in_filename: &str, in_config: &str, out_filename: &str) -> std::io::Result<()> {
     let mut html_output = String::new();
 
     // Open file and read it to string
     let contents = fs::read_to_string(in_filename).expect("Unable to read file");
+    let config_contents = fs::read_to_string(in_config).expect("Unable to read configuration file");
 
     // Parse the markdown contents to HTML
     let mut options = Options::empty();
@@ -31,8 +81,18 @@ fn markdown(in_filename: &str, out_filename: &str) -> std::io::Result<()> {
     let parser = Parser::new_ext(&*contents, options);
     html::push_html(&mut html_output, parser);
 
+    // Parse the configuration file from TOML
+    let config: Config = toml::from_str(&*config_contents).unwrap();
+
+    let start_html = format!("<!DOCTYPE html><html lang=\"{}\"><head><title>{}</title></head><body>", config.language, config.title);
+    let end_html = "</body></html>";
+
+    let mut full_html_output = start_html;
+    full_html_output.push_str(&*html_output);
+    full_html_output.push_str(end_html);
+
     // Write html_output to file
-    fs::write(out_filename, html_output).expect("Unable to write file");
+    fs::write(out_filename, full_html_output).expect("Unable to write file");
 
     Ok(())
 }
