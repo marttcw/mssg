22ff55566c9a0fd75162ffec84853fffa6979cd0
mtcw99 <15316579+mtcw99@users.noreply.github.com>
2021-03-03 16:54:29 +0000

    Rust rewrite initial commit

--- src/log.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOG_H
-#define LOG_H
-
-void log_set_file(const char *filepath);
-void log_deinit(void);
-
-#endif // LOG_H
-
--- src/minify.h
+++
@@ -1,9 +0,0 @@
-#ifndef MINIFY_H
-#define MINIFY_H
-
-#include <stdio.h>
-
-void minify(FILE *out_stream, FILE *in_stream, const char *ext);
-
-#endif // MINIFY_H
-
